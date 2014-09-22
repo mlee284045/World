8ba94cd2b3210201fa94fd7940997167faa8dfda
Mike Lee <mike.seung.lee@gmail.com>
2014-09-22 14:20:42 -0700

    testing git commands

--- search_game/forms.py
+++ search_game/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from search_game.models import City
 
 
 class CreateSearch(UserCreationForm):
@@ -14,3 +15,12 @@ class CreateSearch(UserCreationForm):
             'password1',
             'password2',
         ]
+
+def get_city_choices():
+    return City.objects.all()
+
+class FindCity(forms.Form):
+    name = forms.ChoiceField(choices=get_city_choices())
+    test = forms.CharField(max_length=40)
+
+
--- search_game/views.py
+++ search_game/views.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 from django.shortcuts import render, redirect
 from search_game.models import City, Balance
 from world import settings
-from search_game.forms import CreateSearch
+from search_game.forms import CreateSearch, FindCity
 
 # Create your views here.
 
@@ -29,8 +29,15 @@ def profile(request):
     return render(request, 'profile.html')
 
 def map(request):
-    rocket_space = City.objects.filter(name='San Francisco')[1]
-    data = {'city': rocket_space}
+    rocket_space = City.objects.get(name='San Francisco')
+    data = {'current': None, 'destination': None}
+    if request.method == 'POST':
+        form = FindCity(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data['name']
+            data = {'current': rocket_space, 'destination': city}
+    else:
+        data = {'current': rocket_space, 'destination': None}
     return render(request, 'map.html', data)
 
 def city_view(request, city_id):
