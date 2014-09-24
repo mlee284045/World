9116593aa576c11becd8190a24e54d2eb114f75c
Mike Lee <mike.seung.lee@gmail.com>
2014-09-24 08:20:13 -0700

    still configuring static files

--- world/settings.py
+++ world/settings.py
@@ -101,11 +101,12 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..'))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 try:
